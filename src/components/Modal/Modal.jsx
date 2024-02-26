f438debd908ccf6bed1625abedb7c0d473d41e51
Vitaliya Ovechko <ovechko.vita@gmail.com>
2024-02-26 21:41:21 +0200

    add modal and trips list

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,60 @@
 import { IoClose } from "react-icons/io5";
-import { IoIosArrowDown } from "react-icons/io";
+import styles from "./Modal.module.css";
+import cities from "../../helpers/cities.json";
 
 const Modal = () => {
   return (
-    <div>
-      <div>
-        <p>Create trip</p>
-        <IoClose />
+    // <div className={styles.backdrop}>
+    <div className={styles.modal}>
+      <div className={styles.modalHeader}>
+        <p className={styles.modalTitle}>Create trip</p>
+        <IoClose size={26} fill="#d4d0d0" />
       </div>
       <form>
-        <label htmlFor="city">City</label>
-        <input type="text" placeholder="Please select a city" id="city" />
-        <IoIosArrowDown />
-        <label htmlFor="startDate">Start date</label>
-        <input type="date" placeholder="Select date" id="startDate" />
-        <label htmlFor="endDate">End date</label>
-        <input type="date" placeholder="Select date" id="endDate" />
-        <button>Cancel</button>
-        <button>Save</button>
+        <div className={styles.modalFormElement}>
+          <label className={styles.formLabel} htmlFor="city">
+            City
+          </label>
+          <select className={styles.formInput} type="text" id="city">
+            <option value="" disabled selected hidden>
+              Please select a city
+            </option>
+            {cities.map(({ id, name }) => (
+              <option key={id} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.modalFormElement}>
+          <label className={styles.formLabel} htmlFor="startDate">
+            Start date
+          </label>
+          <input
+            className={styles.formInput}
+            type="date"
+            placeholder="Select date"
+            id="startDate"
+          />
+        </div>
+        <div className={styles.modalFormElement}>
+          <label className={styles.formLabel} htmlFor="endDate">
+            End date
+          </label>
+          <input
+            className={styles.formInput}
+            type="date"
+            placeholder="Select date"
+            id="endDate"
+          />
+        </div>
+        <div className={styles.formBtnWrapper}>
+          <button className={styles.cancelBtn}>Cancel</button>
+          <button className={styles.saveBtn}>Save</button>
+        </div>
       </form>
     </div>
+    // </div>
   );
 };
 export default Modal;
--- src/components/TripsList/TripsList.jsx
+++ src/components/TripsList/TripsList.jsx
@@ -1,28 +1,38 @@
 import trips from "../../helpers/trips.json";
 import { IoSearch } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa";
+import styles from "./TripsList.module.css";
 
 const TripsList = () => {
   return (
     <div>
-      <div>
-        <IoSearch />
-        <input />
+      <div className={styles.searchBox}>
+        <div className={styles.searchIcon}>
+          <IoSearch />
+        </div>
+        <input
+          className={styles.searchInput}
+          type="text"
+          placeholder="Search your trip"
+        />
       </div>
-      <ul>
+      <ul className={styles.tripsList}>
         {trips.map((trip) => (
-          <li key={trip.id}>
-            <p>{trip.city}</p>
-            <p>
-              {trip.startDate}-{trip.endDate}
-            </p>
+          <li key={trip.id} className={styles.tripsListItem}>
+            <img src={trip.photo_url} width={180} height={180}></img>
+            <div className={styles.itemThumb}>
+              <p>{trip.city}</p>
+              <p className={styles.tripDates}>
+                {trip.startDate} - {trip.endDate}
+              </p>
+            </div>
           </li>
         ))}
+        <li>
+          <FaPlus />
+          <p>Add trip</p>
+        </li>
       </ul>
-      <div>
-        <FaPlus />
-        <p>Add trip</p>
-      </div>
     </div>
   );
 };
